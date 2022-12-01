09d1774381ca9ea4207dc226926d9695375322cb
Simon Bjerre <bjerrecs@protonmail.com>
2022-12-01 21:29:50 +0100

    work on pilot stats script

--- pilot_stats.ps1
+++ pilot_stats.ps1
@@ -1,7 +1,13 @@
 # User Configurable
-$config_callsign = "SAS29A"
+$config_callsign = "QFA10"
 
 $request = "https://data.vatsim.net/v3/vatsim-data.json"
-$data = (Invoke-WebRequest $request | ConvertFrom-Json).pilots | Where-Object callsign -eq "AAL407" | Select-Object callsign, altitude, groundspeed, transponder, heading
+$data = (Invoke-WebRequest $request | ConvertFrom-Json).pilots | Where-Object callsign -eq $config_callsign | Select-Object *
+$altitude = $data.altitude
+$groundspeed = $data.groundspeed
+$squak = $data.transponder
+$hdg = $data.heading
+$qnh = $data.qnh_mb
 $flight_plan = $data.flight_plan
+
 Write-Host $flight_plan
